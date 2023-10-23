7ad362e184d7a8d83e637cb74d9af25e7dd9b563
LazizBek <lazizbek.fayzullayev.17@gmail>
2023-10-23 17:44:06 +0500

    i don't know

--- src/model/Task1.java
+++ src/model/Task1.java
@@ -8,15 +8,15 @@ package model;
  * comment 4
  * comment 5
  * 
- * comment 6
- * comment 7
- * comment 8
- * 
  */
 public class Task1 {
 
     public static void main(String[] args) {
         System.out.println("jdjjddjdj");
-        System.out.println("made in branch feature1");
+
+    }
+
+    private void print() {
+
     }
 }
