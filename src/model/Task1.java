936021744eed575803574c6adfeb726654a21239
LazizBek <lazizbek.fayzullayev.17@gmail>
2023-10-23 17:10:47 +0500

    second commit in master

--- src/model/Task1.java
+++ src/model/Task1.java
@@ -4,12 +4,14 @@ package model;
  * Task1
  * comment 1
  * comment 2
+ * comment 3
+ * comment 4
+ * comment 5
  * 
  */
 public class Task1 {
 
     public static void main(String[] args) {
         System.out.println("jdjjddjdj");
-        // ddwdwdwdwd
     }
 }
