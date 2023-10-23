9a785a1c79d9572314004e347628028b831c2c3b
LazizBek <lazizbek.fayzullayev.17@gmail>
2023-10-23 17:06:33 +0500

    first commit in master

---
+++ src/model/Task1.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Task1
+ * comment 1
+ * comment 2
+ * 
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println("jdjjddjdj");
+        // ddwdwdwdwd
+    }
+}
